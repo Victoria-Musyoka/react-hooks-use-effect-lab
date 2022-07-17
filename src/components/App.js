7f2c50a956bf7cc404dd4f6b41ef1bac72a242f2
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-07-17 13:20:14 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,51 @@
 import React, { useState } from "react";
 import Question from "./Question";
-import quiz from "../data/quiz";
+import questions from "../data/quiz";
 
 function App() {
-  const [questions, setQuestions] = useState(quiz);
+  // const [questions, setQuestions] = useState(quiz);
+  const NUMBER_OF_QUESTIONS = questions.length;
   const [currentQuestionId, setCurrentQuestion] = useState(1);
   const [score, setScore] = useState(0);
   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+  
 
   function handleQuestionAnswered(correct) {
-    if (currentQuestionId < questions.length) {
-      setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
-    } else {
-      setCurrentQuestion(null);
-    }
-    if (correct) {
-      setScore((score) => score + 1);
-    }
+      if (currentQuestionId < NUMBER_OF_QUESTIONS) {
+          setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
+      } else {
+          setCurrentQuestion(null);
+      }
+      if (correct) {
+          setScore((score) => score + 1);
+      }
   }
 
+
+  function handlePlayAgain() {
+      setCurrentQuestion(1);
+      setScore(0);
+  }
+  
   return (
-    <main>
+      <main>
       <section>
-        {currentQuestion ? (
-          <Question
-            question={currentQuestion}
-            onAnswered={handleQuestionAnswered}
-          />
-        ) : (
-          <>
-            <h1>Game Over</h1>
-            <h2>Total Correct: {score}</h2>
-          </>
-        )}
+          {currentQuestion ? (
+              <Question
+                  question={currentQuestion}
+                  onAnswered={handleQuestionAnswered}
+              />
+          ) 
+          : 
+          (
+              <>
+                  <h1>Game Over</h1>
+                  <h2>Total Correct: {score}</h2>
+                  <button onClick={handlePlayAgain}>Play again</button>
+              </>
+          )}
       </section>
-    </main>
+      </main>
   );
 }
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,31 +1,56 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 
 function Question({ question, onAnswered }) {
-  const [timeRemaining, setTimeRemaining] = useState(10);
-
+  const MAX_TIME = 3;
+  const [timeRemaining, setTimeRemaining] = useState(MAX_TIME);
+  const { id, prompt, answers, correctIndex } = question;
+  
   // add useEffect code
-
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
+  useEffect(() => {
+      const timeOutId = setTimeout(() => {
+          manageTime();
+      }, 1000);
+      return () => {
+          clearTimeout(timeOutId);
+      }
+  // eslint-disable-next-line react-hooks/exhaustive-deps    
+  });
+  
+  
+  
+  function manageTime() {
+      if (timeRemaining > 1) {
+          setTimeRemaining(timeRemaining => timeRemaining - 1);
+      } else {
+          // When the time runs out, restart the timer and mark the question unanswered
+          setTimeRemaining(MAX_TIME);
+          onAnswered(false);
+      }
   }
 
-  const { id, prompt, answers, correctIndex } = question;
+  
+
+  function handleAnswer(isCorrect) {
+      setTimeRemaining(MAX_TIME);
+      onAnswered(isCorrect);
+  }
+
 
   return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
+      <>
+          <h1>Question {id}</h1>
+          <h3>{prompt}</h3>
+          {answers.map((answer, index) => {
+              const isCorrect = index === correctIndex;
+              return (
+              <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+                  {answer}
+              </button>
+              );
+          })}
+
+          <h5>{timeRemaining} seconds remaining</h5>
+      </>
   );
 }
 
